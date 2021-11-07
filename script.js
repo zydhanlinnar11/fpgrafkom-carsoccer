122590dfaae436b25aae5e30c94e2007af54ec76
mirzaq19 <mirzaqarjap@gmail.com>
2021-11-07 21:22:49 +0700

    add: put some model
    Co-authored-by: Zydhan Linnar Putra <zydhanlinnar11@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let camera, scene, renderer, sizes, orbitControls, ground;
+let camera,
+  scene,
+  renderer,
+  sizes,
+  orbitControls,
+  ground,
+  lighting = [];
 
 const init = () => {
   //========== Canvas
@@ -6,7 +12,6 @@ const init = () => {
 
   //========== Buat Scene
   scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2(0xbffffb, 0.003);
   scene.background = new THREE.Color(0xbffffb);
 
   //========= Pengaturan Camera
@@ -14,8 +19,8 @@ const init = () => {
     width: 0.9 * window.innerWidth,
     height: 0.9 * window.innerHeight,
   };
-  camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 2000);
-  camera.position.set(0, 10, 30);
+  camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 4000);
+  camera.position.set(0, 150, 200);
 
   //========= Orbit Controls
   orbitControls = new THREE.OrbitControls(camera, canvas);
@@ -29,21 +34,52 @@ const init = () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
   //========== Create Lighting
-  scene.add(new THREE.DirectionalLight(0xffffff, 0.8));
-  scene.add(new THREE.AmbientLight(0xd1fff0, 0.4));
+  const dirLight1 = new THREE.DirectionalLight(0xffffff, 8);
+  dirLight1.position.y = 1000;
+  dirLight1.position.x = 10000;
+  const dirLight2 = new THREE.DirectionalLight(0xffffff, 20);
+  dirLight2.position.y = 1600;
+  // ambLight1.position.y = 30;
+  lighting.push(dirLight1);
+  lighting.push(dirLight2);
+
+  lighting.forEach((light) => {
+    scene.add(light);
+  });
+  // scene.add(new THREE.AmbientLight(0xffffff, 0.7));
 
   //========== Create Geometry
   ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(200, 200),
-    new THREE.MeshPhongMaterial({
-      color: 0x262424,
+    new THREE.PlaneGeometry(1600, 1600),
+    new THREE.MeshBasicMaterial({
+      color: 0x348c31,
       side: THREE.DoubleSide,
-      shininess: 0,
     })
   );
   ground.rotation.x = -Math.PI / 2;
   ground.position.y = -2;
   scene.add(ground);
+
+  let loader = new THREE.GLTFLoader();
+  loader.load("model/Car1/scene.gltf", (gltf) => {
+    scene.add(gltf.scene);
+  });
+  loader.load("model/Goal/scene.gltf", (gltf) => {
+    gltf.scene.position.z = 850;
+    gltf.scene.position.x = -250;
+    scene.add(gltf.scene);
+  });
+  loader.load("model/Goal/scene.gltf", (gltf) => {
+    gltf.scene.position.z = -850;
+    gltf.scene.position.x = -250;
+    scene.add(gltf.scene);
+  });
+  loader.load("model/Ball/scene.gltf", (gltf) => {
+    gltf.scene.scale.set(0.25, 0.25, 0.25);
+    gltf.scene.position.z = 100;
+    gltf.scene.position.y = 55;
+    scene.add(gltf.scene);
+  });
 
   // ================== Interactive
   // Sizing canvas
