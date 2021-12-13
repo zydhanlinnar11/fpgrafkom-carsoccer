68ee78a2e3894d4143c32bfd2ccbf09884da4bab
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-12-14 00:29:21 +0700

    Optimize car texture performance

--- src/light.ts
+++ src/light.ts
@@ -4,7 +4,7 @@ export default function createLight(
   scene: THREE.Scene,
   pos: { x: number; y: number; z: number }
 ) {
-  const light = new THREE.PointLight(0xffffff, 1.4)
+  const light = new THREE.PointLight(0xffffff, 2)
   light.position.set(pos.x, pos.y, pos.z)
   light.castShadow = true
   scene.add(light)
