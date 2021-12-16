dd5e763b9695737ff3eb814dc3f7df56889d4966
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-12-16 22:59:25 +0700

    Render game in OOP approch

--- src/Game.ts
+++ src/Game.ts
@@ -9,111 +9,138 @@ import cannonDebugger from 'cannon-es-debugger'
 import Octane from './Object/Octane'
 const DEBUG = false
 
-const loadGame = async () => {
-  const scene = new THREE.Scene()
-  const sizeFactor = 0.6
-  const planeSize: PlaneSize = {
-    width: 105 * sizeFactor,
-    height: 68 * sizeFactor,
+export default class Game {
+  private static sizeFactor = 0.6
+  private static planeSize: PlaneSize = {
+    width: 105 * Game.sizeFactor,
+    height: 68 * Game.sizeFactor,
+  }
+  private static WALL_HEIGHT = 40
+  private keyMap: { [id: string]: boolean }
+
+  private renderer: THREE.WebGLRenderer
+  private scene: THREE.Scene
+  private world: CANNON.World
+  private chaseCamPivot: THREE.Object3D<THREE.Event>
+  private ball: Ball
+  private player1Car: Octane
+  private camera: THREE.Camera
+  private clock: THREE.Clock
+  private delta: number
+
+  private constructor(
+    renderer: THREE.WebGLRenderer,
+    scene: THREE.Scene,
+    world: CANNON.World,
+    chaseCamPivot: THREE.Object3D<THREE.Event>,
+    ball: Ball,
+    player1Car: Octane,
+    camera: THREE.Camera
+  ) {
+    this.world = world
+    this.renderer = renderer
+    this.scene = scene
+    this.chaseCamPivot = chaseCamPivot
+    this.ball = ball
+    this.player1Car = player1Car
+    this.camera = camera
+    this.keyMap = {}
+    this.clock = new THREE.Clock()
+    console.log(this.keyMap)
   }
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  )
-  const { chaseCam, chaseCamPivot } = createChaseCam(scene)
-
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl'),
-  })
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
-
-  const world = new CANNON.World()
-  world.gravity.set(0, -9.82, 0)
-
-  const WALL_HEIGHT = 40
-  createLight(scene, { x: 0, y: WALL_HEIGHT / 2, z: 0 })
-  createPlane(scene, world, planeSize)
-  spawnFourWall(scene, world, planeSize, WALL_HEIGHT)
-  createWall(
-    scene,
-    world,
-    { x: planeSize.width, y: 1, z: planeSize.height },
-    { x: 0, y: WALL_HEIGHT, z: 0 }
-  )
-  const ball = await Ball.createBallInstance(scene, world, {
-    x: 0,
-    y: 1.5,
-    z: 0,
-  })
-
-  const vehicle = await Octane.createCarInstance(
-    scene,
-    world,
-    {
-      x: -3,
-      y: 1,
+  static async createGameInstance(
+    renderer: THREE.WebGLRenderer,
+    camera: THREE.Camera
+  ) {
+    const world = new CANNON.World()
+    const scene = new THREE.Scene()
+    const { chaseCam, chaseCamPivot } = createChaseCam(scene)
+    const ball = await Ball.createBallInstance(scene, world, {
+      x: 0,
+      y: 1.5,
       z: 0,
-    },
-    chaseCam
-  )
-  const keyMap: { [id: string]: boolean } = {}
-  const onDocumentKey = (e: KeyboardEvent) =>
-    (keyMap[e.key] = e.type === 'keydown')
+    })
+    const player1Car = await Octane.createCarInstance(
+      scene,
+      world,
+      {
+        x: -6,
+        y: 1,
+        z: 0,
+      },
+      chaseCam
+    )
 
-  document.addEventListener('keydown', onDocumentKey, false)
-  document.addEventListener('keyup', onDocumentKey, false)
+    world.gravity.set(0, -9.82, 0)
+    createLight(scene, { x: 0, y: Game.WALL_HEIGHT / 2, z: 0 })
+    createPlane(scene, world, Game.planeSize)
+    spawnFourWall(scene, world, Game.planeSize, Game.WALL_HEIGHT)
+    createWall(
+      scene,
+      world,
+      { x: Game.planeSize.width, y: 1, z: Game.planeSize.height },
+      { x: 0, y: Game.WALL_HEIGHT, z: 0 }
+    )
 
-  window.addEventListener('resize', onWindowResize, false)
-  function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    return new Game(
+      renderer,
+      scene,
+      world,
+      chaseCamPivot,
+      ball,
+      player1Car,
+      camera
+    )
   }
 
-  const clock = new THREE.Clock()
-  let delta
+  inputHandler = (e: KeyboardEvent) => {
+    // console.log(e)
+    // console.log(this.keyMap)
+    // console.log(this)
 
-  if (DEBUG) cannonDebugger(scene, world.bodies)
+    this.keyMap[e.key] = e.type === 'keydown'
+  }
 
-  const v = new THREE.Vector3()
+  animate() {
+    requestAnimationFrame(() => this.animate())
+    document.addEventListener('keydown', this.inputHandler, false)
+    document.addEventListener('keyup', this.inputHandler, false)
 
-  function animate() {
-    requestAnimationFrame(animate)
+    if (DEBUG) cannonDebugger(this.scene, this.world.bodies)
 
-    delta = Math.min(clock.getDelta(), 0.1)
-    world.step(delta)
+    const v = new THREE.Vector3()
+
+    this.delta = Math.min(this.clock.getDelta(), 0.1)
+    this.world.step(this.delta)
 
     // Copy coordinates from Cannon to Three.js
-    ball.update()
-    vehicle.update()
-    if (vehicle) {
-      vehicle.setZeroTorque()
-      if (keyMap['w'] || keyMap['ArrowUp']) vehicle.accelerate()
-      if (keyMap['s'] || keyMap['ArrowDown']) vehicle.reverse()
-      if (keyMap['a'] || keyMap['ArrowLeft']) vehicle.turnLeft()
-      if (keyMap['d'] || keyMap['ArrowRight']) vehicle.turnRight()
-      camera.lookAt(vehicle.getChassis().position)
+    this.ball.update()
+    this.player1Car.update()
+    if (this.player1Car) {
+      this.player1Car.setZeroTorque()
+      // console.log(this.player1Car)
+      if (this.keyMap['w'] || this.keyMap['ArrowUp'])
+        this.player1Car.accelerate()
+      if (this.keyMap['s'] || this.keyMap['ArrowDown'])
+        this.player1Car.reverse()
+      if (this.keyMap['a'] || this.keyMap['ArrowLeft'])
+        this.player1Car.turnLeft()
+      if (this.keyMap['d'] || this.keyMap['ArrowRight'])
+        this.player1Car.turnRight()
+      this.camera.lookAt(this.player1Car.getChassis().position)
     }
 
-    chaseCamPivot.getWorldPosition(v)
+    this.chaseCamPivot.getWorldPosition(v)
     if (v.y < 1) {
       v.y = 1
     }
-    camera.position.lerpVectors(camera.position, v, 0.05)
+    this.camera.position.lerpVectors(this.camera.position, v, 0.05)
 
-    render()
+    this.render()
   }
 
-  function render() {
-    renderer.render(scene, camera)
+  render() {
+    this.renderer.render(this.scene, this.camera)
   }
-
-  animate()
 }
-
-export default loadGame
